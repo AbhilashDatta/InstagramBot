94ef6a7df2a82e4adab06853a6a4d7a4c7c33e9a
AbhilashDatta <abhilashdatta@iitkgp.ac.in>
2021-07-17 14:48:08 +0530

    updated

--- __follow_user__.py
+++ __follow_user__.py
@@ -14,6 +14,10 @@ def Follow_user(driver, user):
 
     userclick = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/div[3]/div/div[2]/div/div/a/div'))).click()
     
-    follow = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="react-root"]/section/main/div/header/section/div[1]/div[1]/div/div/button'))).click()
+    try:
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="react-root"]/section/main/div/header/section/div[1]/div[1]/div/div/div/span/span[1]/button'))).click()
+    
+    except:
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="react-root"]/section/main/div/header/section/div[1]/div[1]/div/div/button'))).click()
     
     
