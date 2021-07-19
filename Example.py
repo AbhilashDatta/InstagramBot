9b1dc264c962e354e25ec7df6bd51bcfbcef6ad0
AbhilashDatta <abhilashdatta@iitkgp.ac.in>
2021-07-19 23:58:26 +0530

    updated

--- Example.py
+++ Example.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
     ''' Feed the program your required data'''
 
     bot = Bot()
-    bot.login('user1', 'pw1')
+    bot.login('testaccount3228', 'ta3228')
     bot.dm('abhilash.datta','Hi There')
     bot.follow_users('abhilash.datta')
     bot.like_by_keyword('abhilash.datta', 20)
@@ -13,4 +13,6 @@ if __name__ == '__main__':
     bot.multiple_dm(['abhilash.datta','user2'],'Final Testing')
     bot.retrieve_messages(['abhilash.datta','user2'])
     bot.download_pics('#dog')
+    bot.multiple_dm_from_csv('users.csv','general message')
+    bot.retrieve_messages_from_csv('users.csv')
     bot.logout()
