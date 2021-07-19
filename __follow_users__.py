79427174b00af6dc16bcea8fb5bb85443bf79e7e
AbhilashDatta <abhilashdatta@iitkgp.ac.in>
2021-07-19 14:55:44 +0530

    exceptions handled

--- __follow_users__.py
+++ __follow_users__.py
@@ -13,7 +13,7 @@ def Follow_users(driver, users):
         users = [users]
 
     for user in users:
-        search_user = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/input')))
+        search_user = WebDriverWait(driver, 15).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/input')))
         search_user.send_keys(user)
 
         userclick = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/div[3]/div/div[2]/div/div/a/div'))).click()
@@ -26,4 +26,5 @@ def Follow_users(driver, users):
                 WebDriverWait(driver, 1).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="react-root"]/section/main/div/header/section/div[1]/div[1]/div/div/button'))).click()
             except:
                 WebDriverWait(driver,1).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="react-root"]/section/main/div/header/section/div[1]/div[2]/div/div/div/span/span[1]/button'))).click()
-    
+
+        driver.get('https://www.instagram.com/')
